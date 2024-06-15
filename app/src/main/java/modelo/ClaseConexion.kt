e81d3f0f6bdb4af939427804a38cdbeaa5982125
Jonathan <Jonathan>
2024-06-14 19:01:08 -0600

    Creacion de conexion

---
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -0,0 +1,22 @@
+package modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+
+    fun cadenaConexion(): Connection? {
+        try{
+            val url = "jdbc:oracle:thin:@192.168.0.20:1521:xe"
+            val usuario = "system"
+            val contrasena = "ITR2024"
+
+            val connection = DriverManager.getConnection(url, usuario, contrasena)
+            return connection
+        }catch (error:Exception) {
+            println("Este es el error: $error")
+            return null
+        }
+    }
+
+}
