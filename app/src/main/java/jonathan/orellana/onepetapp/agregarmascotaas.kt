140463ae695ee672d1cb401f887259b8483c8a63
Fernando Morales <20230032@ricaldone.edu.sv>
2024-07-08 15:45:06 -0600

    Agregar mascotas avance

--- app/src/main/java/jonathan/orellana/onepetapp/agregarmascotaas.kt
+++ app/src/main/java/jonathan/orellana/onepetapp/agregarmascotaas.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.ClaseConexion
+import modelo.dataClassEspecie
+import java.util.UUID
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +25,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [agregarmascotas.newInstance] factory method to
+ * Use the [agregarmascotaas.newInstance] factory method to
  * create an instance of this fragment.
  */
-class agregarmascotas : Fragment() {
+class agregarmascotaas : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,11 +39,7 @@ class agregarmascotas : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
-
-    //1-Mando a llamar todos los elementos de la vista para programarlos
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,25 +49,64 @@ class agregarmascotas : Fragment() {
         val root = inflater.inflate(R.layout.fragment_agregarmascotas, container, false)
         //Mando a llamar el boton usando la variable root
         val txtNomAddMascota = root.findViewById<EditText>(R.id.txtNombreMascota)
-        val txtAddEspecie = root.findViewById<EditText>(R.id.txtEspecieMascota)
+        val spSelectEspecie = root.findViewById<Spinner>(R.id.spEspecie)
         val txtGenAddMascota = root.findViewById<EditText>(R.id.txtGeneroMascota)
         val txtPesoAddMascota = root.findViewById<EditText>(R.id.txtPesoMascota)
         val txtAñoAddMascota = root.findViewById<EditText>(R.id.txtAñoMascota)
         val txtEnferCAddMascota = root.findViewById<EditText>(R.id.txtEnfermedadesCMascota)
         val txtProceMAddMascota = root.findViewById<EditText>(R.id.txtProcedimientosMAscota)
+        val txtRazaMascota = root.findViewById<EditText>(R.id.txtRazaMascota)
         val txtAlerAddMascota = root.findViewById<EditText>(R.id.txtAlergiasMascota)
         val btnAddFotoMascota = root.findViewById<Button>(R.id.btnAgregarFotoMascota)
         val btnAgregarMascotas = root.findViewById<Button>(R.id.btnAgregarMascotas)
 
+        //1-Creamos la funcion que haga un select
+        fun obtenerEspecie(): List<dataClassEspecie> {
+            //Creo un objeto de la clase Conexion
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            //Crear un Statement que ejecute el Select
+            val statement = objConexion?.createStatement()
+            val resultSet = statement?.executeQuery("Select * from tbEspecies")!!
+
+            val listaEspecie = mutableListOf<dataClassEspecie>()
+
+            while (resultSet.next()) {
+                val uuidEspecie = resultSet.getString("UUID_especie")
+                val nombreEspecie = resultSet.getString("nombre_especie")
+
+                val especieCompleta = dataClassEspecie(uuidEspecie, nombreEspecie)
+                listaEspecie.add(especieCompleta)
+            }
+            return listaEspecie
+        }
+
+        //Prgramar al Spinner
+        CoroutineScope(Dispatchers.IO).launch {
+            //1-Obtengo el listado de datos que quiero mostrar
+            val listadoDeEspecie = obtenerEspecie()
+            val nombreEspecies = listadoDeEspecie.map { it.nombre_especie }
+            withContext(Dispatchers.Main) {
+                //2- Creo y configuro el adaptador
+                val miAdaptador = ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    nombreEspecies
+                )
+                spSelectEspecie.adapter = miAdaptador
+            }
+        }
+
+
         btnAgregarMascotas.setOnClickListener{
             //Guardar en una variable los valores que escribio el  usuario
             val nombreMascota = txtNomAddMascota.text.toString()
-            val especieMascota = txtAddEspecie.text.toString()
             val generoMascota = txtGenAddMascota.text.toString()
             val pesoMascota = txtPesoAddMascota.text.toString()
             val añoMascota = txtAñoAddMascota.text.toString()
             val enfermedadesCronicas = txtEnferCAddMascota.text.toString()
             val procedimientoMA = txtProceMAddMascota.text.toString()
+            val razaMascota = txtRazaMascota.text.toString()
             val alergiaMascota = txtAlerAddMascota.text.toString()
 
             //Variable para verificar si hay errores la inicializamos en false
@@ -77,14 +120,6 @@ class agregarmascotas : Fragment() {
             }
             else {
                 txtNomAddMascota.error = null
-            }
-            //Especie Mascota
-            if (especieMascota .isEmpty()) {
-                txtAddEspecie.error = "La especie de la Mascota es obligatorio"
-                hayErrores = true
-            }
-            else {
-                txtAddEspecie.error = null
             }
             //Genero de Mascota
             if (generoMascota.isEmpty()) {
@@ -126,6 +161,14 @@ class agregarmascotas : Fragment() {
             else {
                 txtProceMAddMascota.error = null
             }
+            //Raza Mascota
+            if (razaMascota.isEmpty()) {
+                txtRazaMascota.error = "El apartado es obligatorio llenarse"
+                hayErrores = true
+            }
+            else {
+                txtProceMAddMascota.error = null
+            }
             //Alergias Mascota
             if (alergiaMascota.isEmpty()) {
                 txtAlerAddMascota.error = "El apartado es obligatorio llenarse"
@@ -144,48 +187,55 @@ class agregarmascotas : Fragment() {
                 txtPesoAddMascota.error = null
             }
 
-            // Función para limpiar los campos
-            fun limpiarCampos() {
-                txtNomAddMascota.text.clear()
-                txtAddEspecie.text.clear()
-                txtGenAddMascota.text.clear()
-                txtPesoAddMascota.text.clear()
-                txtAñoAddMascota.text.clear()
-                txtEnferCAddMascota.text.clear()
-                txtProceMAddMascota.text.clear()
-                txtAlerAddMascota.text.clear()
-            }
-
-            // Función para guardar los datos
-            fun guardarDatos(
-                nombreMascota: String,
-                especieMascota: String,
-                generoMascota: String,
-                pesoMascota: Int,
-                añoMascota: String,
-                enfermedadesCronicas: String,
-                procedimientoMA: String,
-                alergiasMascota: String
-            ) {
-                // Lógica para guardar los datos
-                // Aqui tendría que ir lo de oracle
-                Toast.makeText(context, "Datos guardados", Toast.LENGTH_SHORT).show()
-            }
-
             //Si hay errores no procede a guardar los datos
             if(hayErrores) {
                 //Hacer algo si hay errores
             }
             else {
                 //Si todas las validaciones son correcta, procede a guardar los daots
-                guardarDatos(nombreMascota, especieMascota, generoMascota, pesoMascota.toInt(), añoMascota, enfermedadesCronicas, procedimientoMA, alergiaMascota)
-                limpiarCampos()
+                CoroutineScope(Dispatchers.IO).launch {
+                    val especiee = obtenerEspecie()
+                    val especieID = especiee[spSelectEspecie.selectedItemPosition].UUID_especie
+                    //Guardar datos
+                    //1- Creo un objeto de la clase conexion
+                    val claseC = ClaseConexion().cadenaConexion()
+
+                    //2- creo una variable que contenga un PrepareStatement
+                    val addMascota =
+                        claseC?.prepareStatement("into tbMascotas(uuid_mascota, nombre_mascota, raza, sexo, procesos_previos, alergias, enfermedades_cronicas, fecha_nacimiento, peso,  especie, dueno) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )")!!
+                    addMascota.setString(1, UUID.randomUUID().toString())
+                    addMascota.setString(2, txtNomAddMascota.text.toString())
+                    addMascota.setString(3, txtRazaMascota.text.toString())
+                    addMascota.setString(4, txtGenAddMascota.text.toString())
+                    addMascota.setString(5, txtProceMAddMascota.text.toString())
+                    addMascota.setString(6, txtAlerAddMascota.text.toString())
+                    addMascota.setString(7, txtEnferCAddMascota.text.toString())
+                    addMascota.setString(8, txtAñoAddMascota.text.toString())
+                    addMascota.setInt(9, txtPesoAddMascota.text.toString().toInt())
+                    //FALTA DUEÑO
+                    addMascota.setString(2, especieID)
+                    addMascota.executeUpdate()
+
+                    //Abro una corrutina para mostrar una alerta y limpiar los campos
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(context, "Datos guardados", Toast.LENGTH_SHORT).show()
+                        txtNomAddMascota.setText("")
+                        txtRazaMascota.setText("")
+                        txtGenAddMascota.setText("")
+                        txtGenAddMascota.setText("")
+                        txtProceMAddMascota.setText("")
+                        txtAlerAddMascota.setText("")
+                        txtEnferCAddMascota.setText("")
+                        txtEnferCAddMascota.setText("")
+                        txtAñoAddMascota.setText("")
+                        txtPesoAddMascota.setText("")
+                    }
+                }
             }
         }
         //retornar root
         return root
     }
-
 
     companion object {
         /**
@@ -194,17 +244,16 @@ class agregarmascotas : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment agregarmascotas.
+         * @return A new instance of fragment agregarmascotaas.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            agregarmascotas().apply {
+            agregarmascotaas().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-
     }
 }
