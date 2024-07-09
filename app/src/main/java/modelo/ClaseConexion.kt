bdd1de60ebddead33bad31028243d60c08dc04e0
Fernando Morales <20230032@ricaldone.edu.sv>
2024-07-09 15:29:05 -0600

    Pequeños Cambios

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -7,7 +7,7 @@ class ClaseConexion {
 
     fun cadenaConexion(): Connection? {
         try{
-            val url = "jdbc:oracle:thin:@192.168.153.1201:xe"
+            val url = "jdbc:oracle:thin:@192.168.153.120:1521:xe"
             val usuario = "FERNANDO_DEVELOPER"
             val contrasena = "4O32PcEI"
 
