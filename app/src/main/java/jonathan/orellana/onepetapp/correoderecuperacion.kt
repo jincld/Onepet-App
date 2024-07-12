3d58eea877f200a143765ad57cec3f4f4e82d00d
Fernanda <20220031@ricaldone.edu.sv>
2024-07-12 10:45:15 -0600

    Primeros cambios recuperacion

--- app/src/main/java/jonathan/orellana/onepetapp/correoderecuperacion.kt
+++ app/src/main/java/jonathan/orellana/onepetapp/correoderecuperacion.kt
@@ -3,13 +3,22 @@ package jonathan.orellana.onepetapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class correoderecuperacion : AppCompatActivity() {
+
+    companion object globalvariables {
+        lateinit var numeroaleatorio : String
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,13 +29,41 @@ class correoderecuperacion : AppCompatActivity() {
             insets
         }
 
-        val correorecuperacion = findViewById<TextView>(R.id.txtcorreorecupracion)
+        val correorecuperacion = findViewById<EditText>(R.id.txtcorreorecupracion)
         val btnrecupercion = findViewById<Button>(R.id.btncorreorecuperacion)
+
+
 
        btnrecupercion.setOnClickListener {
 
-            val recuperarcorreo = Intent(this, codigoconfimarcion::class.java)
-            startActivity(recuperarcorreo)
+           val correo = correorecuperacion.text.toString()
+           var hayerrores = false
+
+           if (!correo.matches(Regex("[a-zA-Z0-9._-]+@[a-z]+[.][a-z]+"))){
+
+               correorecuperacion.error = "Ingrese un correo valido"
+               hayerrores = true
+           } else {
+               correorecuperacion.error = null
+           }
+           if (hayerrores){
+           } else {
+try {
+
+
+               CoroutineScope(Dispatchers.Main).launch {
+                   numeroaleatorio = (1000..10000).random().toString()
+                   enviarCorreo("$correo", "Codigo de recuperacion", "Codigo de recuperacion, No olvide su contrasña $numeroaleatorio")
+
+                   println("este es el coresrasfasdf $correo")
+               }
+}catch (e: Exception){
+    println("este es el eroaeroasdf $e")
+    Toast.makeText(this, "aasdfdsf $e", Toast.LENGTH_SHORT).show()
+              // val recuperarcorreo = Intent(this, codigoconfimarcion::class.java)
+              // startActivity(recuperarcorreo)
+           }
+           }
         }
     }
 }
--- app/src/main/java/jonathan/orellana/onepetapp/nuevacontrasena.kt
+++ app/src/main/java/jonathan/orellana/onepetapp/nuevacontrasena.kt
@@ -3,11 +3,17 @@ package jonathan.orellana.onepetapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.ClaseConexion
 
 class nuevacontrasena : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +26,33 @@ class nuevacontrasena : AppCompatActivity() {
             insets
         }
 
-        val txtnuevacontra = findViewById<TextView>(R.id.txtnuevacontra)
+
+
+        val txtnuevacontra = findViewById<EditText>(R.id.txtnuevacontra)
         val btnnuevacontra = findViewById<Button>(R.id.btnnuevacontra)
 
-        btnnuevacontra.setOnClickListener {
+        fun actualizardatos(nuevacontra: String){
+            GlobalScope.launch(Dispatchers.IO) {
+                val objConexion = ClaseConexion().cadenaConexion()
 
-            val recuperar = Intent(this, iniciarsesion::class.java)
-            startActivity(recuperar)
+
+                val updatecontra = objConexion?.prepareStatement("update tbUsuariosOne set contra_usuario = ? where correo_usuario = ?")!!
+                updatecontra.setString(1, nuevacontra)
+                updatecontra.executeUpdate()
+
+                val commit = objConexion.prepareStatement("commit")!!
+                commit.executeUpdate()
+
+                withContext(Dispatchers.Main){
+                    actualizardatos(nuevacontra)
+                }
+
+            }
+            btnnuevacontra.setOnClickListener {
+            actualizardatos(nuevacontra)
         }
-    }
+            val recuperar = Intent(this, iniciarsesion::class.java)
+           startActivity(recuperar)
+        }
+   }
 }
