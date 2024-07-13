cb15f2f21d81dd03f4308d0ce7519a874ecc7341
Fernando Morales <20230032@ricaldone.edu.sv>
2024-07-12 22:34:30 -0600

    Avance Mis Mascotas

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -1,4 +1,23 @@
 package RecyclerViewHelpers
 
-class Adaptador {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import jonathan.orellana.onepetapp.R
+import modelo.tbMascotas
+
+class Adaptador (var Datos: List<tbMascotas>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //Unir el RecyclerView con la card
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_cardmascotas, parent, false)
+        return ViewHolder(vista)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
