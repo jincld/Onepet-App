e88d0519aa826bbbea83adeea01c24fbd8decc54
Fernando Morales <20230032@ricaldone.edu.sv>
2024-08-06 14:27:35 -0600

    Intento de Foto

--- app/src/main/java/jonathan/orellana/onepetapp/agregarmascotaas.kt
+++ app/src/main/java/jonathan/orellana/onepetapp/agregarmascotaas.kt
@@ -1,7 +1,13 @@
 package jonathan.orellana.onepetapp
 
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,14 +15,19 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import modelo.ClaseConexion
 import modelo.dataClassEspecie
+import java.io.ByteArrayOutputStream
 import java.util.Calendar
 import java.util.UUID
 
@@ -31,21 +42,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class agregarmascotaas : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     companion object variablesGlobalesMascota{
         lateinit var variableMascotaGlobal: String
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -293,6 +297,4 @@ class agregarmascotaas : Fragment() {
         //retornar root
         return root
     }
-
-    
 }
