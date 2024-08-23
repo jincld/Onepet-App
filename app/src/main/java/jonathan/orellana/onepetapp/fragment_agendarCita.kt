f038a7ac38968c5881c831b9fd683664fc8bda22
Fernando Morales <20230032@ricaldone.edu.sv>
2024-08-22 22:39:25 -0600

    Agendar Cita 2

--- app/src/main/java/jonathan/orellana/onepetapp/fragment_agendarCita.kt
+++ app/src/main/java/jonathan/orellana/onepetapp/fragment_agendarCita.kt
@@ -5,27 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.ClaseConexion
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_agendarCita.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_agendarCita : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var txtFechaCita: EditText
+    private lateinit var spVetCita: Spinner
+    private lateinit var spMascotaCita: Spinner
+    private lateinit var txtMotivoCita: EditText
+    private lateinit var txtDescripcionCita: EditText
+    private lateinit var btnEnviarCita: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -33,27 +36,262 @@ class fragment_agendarCita : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agendar_cita, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_agendarCita.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_agendarCita().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val root = inflater.inflate(R.layout.fragment_agendar_cita, container, false)
+
+        txtFechaCita = root.findViewById(R.id.txtFechaCita)
+        spVetCita = root.findViewById(R.id.spVetCita)
+        spMascotaCita = root.findViewById(R.id.spMascotaCita)
+        txtMotivoCita = root.findViewById(R.id.txtMotivoCita)
+        txtDescripcionCita = root.findViewById(R.id.txtDescripcionCitas)
+        btnEnviarCita = root.findViewById(R.id.btnEnviarCita)
+
+        loadVet()
+        loadMascotas()
+
+        btnEnviarCita.setOnClickListener {
+            val fechaCita = txtFechaCita.text.toString()
+            val vetSeleccionado = spVetCita.selectedItem.toString()
+            val mascotaSeleccionada = spMascotaCita.selectedItem.toString()
+            val motivoCita = txtMotivoCita.text.toString()
+            val descripcionCita = txtDescripcionCita.text.toString()
+
+            CoroutineScope(Dispatchers.Main).launch {
+                val idVetC = getIdVet(vetSeleccionado)
+                val idMascotaC = getIdMascota(mascotaSeleccionada)
+
+                if (idVetC != null && idMascotaC != null) {
+                    val result =
+                        saveEnviarCita(
+                            fechaCita,
+                            idVetC,
+                            idMascotaC,
+                            motivoCita,
+                            descripcionCita
+                        )
+                    if (result) {
+                        Toast.makeText(
+                            requireContext(),
+                            "Asignación guardada correctamente",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        txtFechaCita.setText("")
+                        txtMotivoCita.setText("")
+                        txtDescripcionCita.setText("")
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "Error al guardar la asignación",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error: No se pudo obtener los IDs",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
+        }
+        return root
     }
+
+    private fun loadVet() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val vetc = fetchVetDB()
+            withContext(Dispatchers.Main) {
+                val adapter = ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_spinner_item,
+                    vetc
+                )
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spVetCita.adapter = adapter
+            }
+        }
+    }
+
+    private fun loadMascotas() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val mascotac = fetchMascotaDB()
+            withContext(Dispatchers.Main) {
+                val adapter = ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_spinner_item,
+                    mascotac
+                )
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spMascotaCita.adapter = adapter
+            }
+        }
+    }
+
+    private suspend fun fetchVetDB(): List<String> = withContext(Dispatchers.IO) {
+        val veterinaria = mutableListOf<String>()
+        val query = "SELECT nombre_veterinaria FROM tbVeterinarias"
+        val objConexion = ClaseConexion().cadenaConexion()
+
+        objConexion?.let {
+            try {
+                val statement = it.createStatement()
+                val resultSet = statement.executeQuery(query)
+
+                while (resultSet.next()) {
+                    val nombre_veterinaria = resultSet.getString("nombre_veterinaria")
+                    veterinaria.add("$nombre_veterinaria")
+                }
+
+                resultSet.close()
+                statement.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                it.close()
+            }
+        }
+        veterinaria
+    }
+
+    private suspend fun fetchMascotaDB(): List<String> = withContext(Dispatchers.IO) {
+        val mascotas = mutableListOf<String>()
+        val query = "SELECT nombre_mascota FROM tbMascotas"
+        val objConexion = ClaseConexion().cadenaConexion()
+
+        objConexion?.let {
+            try {
+                val statement = it.createStatement()
+                val resultSet = statement.executeQuery(query)
+
+                while (resultSet.next()) {
+                    val nombre_mascota = resultSet.getString("nombre_mascota")
+                    mascotas.add(nombre_mascota)
+                }
+
+                resultSet.close()
+                statement.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                it.close()
+            }
+        }
+        mascotas
+    }
+
+    private suspend fun getIdVet(nombreVet: String): Int? =
+        withContext(Dispatchers.IO) {
+            val query =
+                "SELECT UUID_Veterinaria FROM tbVeterinarias WHERE nombre_veterinaria =  ?"
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            objConexion?.let {
+                try {
+                    val statement = it.prepareStatement(query)
+                    statement.setString(1, nombreVet)
+                    val resultSet = statement.executeQuery()
+
+                    if (resultSet.next()) {
+                        val idVetC = resultSet.getInt("UUID_Veterinaria")
+                        resultSet.close()
+                        return@withContext idVetC
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    it.close()
+                }
+            }
+            null
+        }
+
+    private suspend fun getIdMascota(nombreMascota: String): Int? =
+        withContext(Dispatchers.IO) {
+            val query = "SELECT UUID_Mascota FROM tbMascotas WHERE nombre_mascota = ?"
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            objConexion?.let {
+                try {
+                    val statement = it.prepareStatement(query)
+                    statement.setString(1, nombreMascota)
+                    val resultSet = statement.executeQuery()
+
+                    if (resultSet.next()) {
+                        val idMascotaC = resultSet.getInt("UUID_Mascota")
+                        resultSet.close()
+                        return@withContext idMascotaC
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    it.close()
+                }
+            }
+            null
+        }
+
+    private suspend fun getIdUsuarioOne(nombre_usuario: String): Int? = withContext(Dispatchers.IO) {
+        val query = "SELECT UUID_Usuario FROM tbUsuarios WHERE nombre_usuario = ?"
+        val objConexion = ClaseConexion().cadenaConexion()
+
+        objConexion?.let {
+            try {
+                val statement = it.prepareStatement(query)
+                statement.setString(1, nombre_usuario)
+                val resultSet = statement.executeQuery()
+
+                if (resultSet.next()) {
+                    val idUsuarioOne = resultSet.getInt("UUID_Usuario")
+                    resultSet.close()
+                    return@withContext idUsuarioOne
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                it.close()
+            }
+        }
+        null
+    }
+
+    // Luego, en el método saveEnviarCita, puedes obtener el ID del usuario y pasararlo como parámetro
+    private suspend fun saveEnviarCita(
+        fecha_cita: String,
+        idVetC: Int,
+        idMascotaC: Int,
+        motivo_cita: String,
+        descripcion_motivo: String
+    ): Boolean = withContext(Dispatchers.IO) {
+        val idUsuarioOne = getIdUsuarioOne("nombre_usuario")
+        if (idUsuarioOne != null) {
+            val query =
+                "INSERT INTO tbCitas (fecha_cita, vet, mascota, usuario, motivo_cita, descripcion_motivo) VALUES (?, ?, ?, ?, ?, ?)"
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            objConexion?.let {
+                try {
+                    val statement = it.prepareStatement(query)
+                    statement.setString(1, fecha_cita)
+                    statement.setInt(2, idVetC)
+                    statement.setInt(3, idMascotaC)
+                    statement.setInt(4, idUsuarioOne)
+                    statement.setString(5, motivo_cita)
+                    statement.setString(6, descripcion_motivo)
+                    statement.executeUpdate()
+                    statement.close()
+                    it.close()
+                    true
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    false
+                } finally {
+                    it.close()
+                }
+            } ?: false
+        } else {
+            false
+        }
+    }
+
 }
